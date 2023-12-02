e3b98fd30bbb7c383449da1a38bc1256297e64c7
rosh <user@Rosh-MacBook-Pro.local>
2023-12-02 18:37:00 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+let slides = document.getElementsByClassName("slides");
+let icons = document.getElementsByClassName("icon");
+let prev = document.querySelector(".prev");
+let next = document.querySelector(".next");
+
+if (!slides.length == 0) {
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  let currentSlide = function (n) {
+    showSlides((slideIndex = n));
+  };
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < icons.length; i++) {
+      icons[i].className = icons[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    icons[slideIndex - 1].className += " active";
+  }
+}
+
+prev.addEventListener("click", () => {
+  plusSlides(-1);
+});
+
+next.addEventListener("click", () => {
+  plusSlides(1);
+});
